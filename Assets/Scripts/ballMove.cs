dcc851ca899e6900b9f1bca10a6dbfa2f0f918d4
Akash Gogate <akash@akashAir-1136.local>
2023-09-30 12:06:16 -0400

    initial commit

---
+++ Assets/Scripts/ballMove.cs
@@ -0,0 +1,82 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ballMove : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (transform.position.z != 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        
+        rb = gameObject.GetComponent<Rigidbody>();
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && transform.position.x<13.5)
+        {
+            rb.AddForce(new Vector3(0.2f, 0f, 0f), ForceMode.Impulse);
+        }
+        else if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && transform.position.x > -13.5)
+        {
+            rb.AddForce(new Vector3(-0.2f, 0f, 0f), ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y<4.25 && transform.position.y < 0.75)
+        {
+            rb.AddForce(new Vector3(0f, 10f,0f), ForceMode.Impulse);
+        }
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y < 1)
+        {
+            GameObject[] obstacles = GameObject.FindGameObjectsWithTag("floor");
+            foreach (GameObject obst in obstacles) {
+                if(obst.transform.position.y == 0)
+                {
+                    obst.transform.position = new Vector3(obst.transform.position.x, 8,obst.transform.position.z);
+                    obst.transform.Rotate(0f, 0f, 180f);
+                }
+                else if (obst.transform.position.y == 8)
+                {
+                    obst.transform.position = new Vector3(obst.transform.position.x, 0, obst.transform.position.z);
+                    obst.transform.Rotate(0f, 0f, 180f);
+                }
+
+                else if (obst.transform.position.y == 0.5)
+                {
+                    obst.transform.position = new Vector3(obst.transform.position.x * (-1f), 7.5f, obst.transform.position.z);
+                    obst.transform.Rotate(0f, 0f, 180f);
+                }
+                
+                else if(obst.transform.position.y == 7.5)
+                {
+                    obst.transform.position = new Vector3(obst.transform.position.x*(-1f), 0.5f, obst.transform.position.z);
+                    obst.transform.Rotate(0f, 0f, 180f);
+                }
+
+            }
+            transform.position = new Vector3(transform.position.x, 7.5f, transform.position.z);
+        }
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "log(Clone)")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/starst.cs
@@ -0,0 +1,89 @@
+
+
+
+using UnityEngine;
+
+public class starst : MonoBehaviour
+{
+
+    public GameObject plane;
+    int count = 2;
+    float timer;
+    public GameObject reference;
+    public GameObject box;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject plane2 = Instantiate(plane, new Vector3(0, 8, 0), Quaternion.identity);
+        plane2.transform.Rotate(0f, 0, 180f);
+        for (int i = 0; i < 2; i++)
+        {
+            Instantiate(plane, new Vector3(0, 0, 3 * i), Quaternion.identity);
+            GameObject plane1 = Instantiate(plane, new Vector3(0, 8, 3 * i), Quaternion.identity);
+            plane1.transform.Rotate(0f, 0f, 180f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float random = Random.Range(-12.5f, 12.5f);
+        float random2 = Random.Range(-12.5f, 12.5f);
+
+        
+
+        timer += Time.deltaTime;
+        if (reference.transform.position.z > -15)
+        {
+            count += 1;
+            Instantiate(plane, new Vector3(0, 0, 3 * count-3), Quaternion.identity);
+            GameObject plane1 = Instantiate(plane, new Vector3(0, 8, 3 * count-3), Quaternion.identity);
+            plane1.transform.Rotate(0f, 0f, 180f);
+            timer = 0;
+        }
+        else
+        {
+            if (timer >= 0.6)
+            {
+                GameObject bottomPlane = Instantiate(plane, new Vector3(0, 0, 3 * count - 3), Quaternion.identity);
+                GameObject topPlane = Instantiate(plane, new Vector3(0, 8, 3 * count - 3), Quaternion.identity);
+                topPlane.transform.Rotate(0f, 0f, 180f);
+
+
+                foreach (Transform transform in topPlane.transform)
+                {
+                    float random3 = Random.Range(0f, 8f);
+                    if (random3 < 3)
+                    {
+                        DestroyImmediate(transform.gameObject);
+                    }
+
+                }
+                foreach (Transform transform in bottomPlane.transform)
+                {
+                    float random3 = Random.Range(0f, 8f);
+                    if (random3 < 2)
+                    {
+                        DestroyImmediate(transform.gameObject);
+                    }
+
+                }
+                float box1 = Random.Range(0f, 8f);
+                float box2 = Random.Range(0f, 8f);
+                if (box1 < 4.5)
+                {
+                    Instantiate(box, new Vector3(random, 0.5f, 3 * count), Quaternion.identity);
+                }
+                if (box2 < 4.5)
+                { 
+                    Instantiate(box, new Vector3(random2, 7.5f, 3 * count), Quaternion.identity);
+                }
+                timer = 0;
+            }
+        }
+        
+        
+    }
+}
